a11799e78eb3a37433a42750aebedd17aa9b0d7e
mbach <bachm.biz@gmail.com>
2019-04-14 18:13:13 +0700

    Add lesson 3

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -1,3 +1,4 @@
+// Specify solidity version
 pragma solidity ^0.4.25;
 
 // Main contract for Zombie creation
@@ -12,10 +13,18 @@ contract ZombieFactory {
     // Create a modulus to convert numbers into base-16
     uint dnaModulus = 10 ** dnaDigits;
 
-    // Define a new Object with 2 attributes
+    // Set standard cooldown time
+    uint cooldownTime = 1 days;
+
+    // Define a new Object with 4 attributes
     struct Zombie {
         string name;
         uint dna;
+        // Zombie that wins more battle level up faster with access to more abilities
+        uint32 level;
+
+        // Time before a zombie can feed/ attack again
+        uint32 readyTime;
     }
 
     // Create an array of the newly created Object
@@ -27,23 +36,45 @@ contract ZombieFactory {
     // Create a key-value binding to return the number of zombie an user owns
     mapping (address => uint) ownerZombieCount;
 
-    // Declare new function to create a Zombie instance with 2 params
+    // Declare new function to create a Zombie instance with 2 params.
+    // Internal function for other inherit contract to re-use this function (not enabled in Private)
     function _createZombie(string _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+
+        // Push the newly created Zombie instance to the array, and declare an id after (= current array length - 1)
+        // The readyTime is passed in as the time a Zombie can feed on its victim again in the future
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
+
+        // Assign the id to the key-value binding
         zombieToOwner[id] = msg.sender;
+
+        // Assign the count to the key-value binding
         ownerZombieCount[msg.sender]++;
+
+        // Trigger the event
         emit NewZombie(id, _name, _dna);
     }
 
+    // Declare a function to generate random DNA based on name of the Zombie
+    // View since this function only take the data and read without modifying the data
     function _generateRandomDna(string _str) private view returns (uint) {
+
+        // Convert the string to uint after encode the string to bytes
         uint rand = uint(keccak256(abi.encodePacked(_str)));
+
+        // Convert to base-16 to qualify to be a Zombie's DNA
         return rand % dnaModulus;
     }
 
+    // Declare a function to generate random DNA and then create a new Zombie based on that DNA
     function createRandomZombie(string _name) public {
+
+        // Only user who never owned a Zombie before can create one 
         require(ownerZombieCount[msg.sender] == 0);
+
+        // Generate a random zombieDna
         uint randDna = _generateRandomDna(_name);
-        randDna = randDna - randDna % 100;
+
+        // Create a random Zombie with the random Dna
         _createZombie(_name, randDna);
     }
 
--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -1,8 +1,13 @@
+// Specify Solidity version
 pragma solidity ^0.4.25;
 
+// Import the contract from other file
 import "./zombiefactory.sol";
 
+// Create new contract for the Interface with other external contracts and functions inside them
 contract KittyInterface {
+
+  // Get the return values of the Kitty with its id being passed in the function
   function getKitty(uint256 _id) external view returns (
     bool isGestating,
     bool isReady,
@@ -13,30 +18,72 @@ contract KittyInterface {
     uint256 matronId,
     uint256 sireId,
     uint256 generation,
-    uint256 genes
+    uint256 genes // The information we want (at index 10)
   );
 }
 
+// Create a new contract to modify the Zombie's features
+// This is an inheritance of the main contract to reuse functions without flooding the script file 
 contract ZombieFeeding is ZombieFactory {
 
+  // The address of an user
   address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
   
-  KittyInterface kittyContract = KittyInterface(ckAddress);
+  // Only declare it without binding to any value (not making it a state variable)
+  KittyInterface kittyContract;
 
+  // Make a function to create an Interface based on a dynamic address
+  function setKittyContractAddress(address _address) external {
+    kittyContract = KittyInterface(_address);
+  }
+
+  // Declare a function to reset the readyTime
+  function _triggerCooldown(Zombie storage _zombie) internal {
+    _zombie.readyTime = uint32(now + cooldownTime);
+  }
+
+  // Check if the Zombie's readyTime has passed yet 
+  function _isReady(Zombie storage _zombie) internal view returns (bool) {
+      return (_zombie.readyTime <= now);
+  }
+
+  // Declare a function to pass the Kitty's info in to modify the Zombie's feature
   function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
+    
+    // Only if the request maker is the Zombie's owner
     require(msg.sender == zombieToOwner[_zombieId]);
+
+    // Get the Zombie at a certain id and store it in the storage, not in the chain
     Zombie storage myZombie = zombies[_zombieId];
+
+    // It's required for the Zombie to passed the readyTime before taking in the newDna again
+    require(_isReady(myZombie));
+
+    // Set the new zombie's DNA
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
+    
+
+    // If the target's species is a kitty, plus 99 to its dna's final 2 digits
     if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
       newDna = newDna - newDna % 100 + 99;
     }
+
+    // Then create the new Zombie as usual 
     _createZombie("NoName", newDna);
+
+    // Reset the readyTime after the Zombie feed on the new kittyDna
+    _triggerCooldown(myZombie);
   }
 
+  // Pass the kitty to the ZombieFeeding machine
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    
+    // Get the value of the Kitty's DNA
     uint kittyDna;
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+
+    // Parse that info to the function including the keyword "kitty"
     feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
 
